256b32f8e9f97689a02f743c24b788034ab6c3be
rohiminzainur <rohiminzainur@gmail.com>
2023-04-10 21:22:37 +0700

    Belajar tailwind css

---
+++ 12belajar_tailwind/latihan9/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      spacing: {
+        '13': '3.25rem'
+      },
+      fontFamily: {
+        inter: ['Inter'],
+      },
+      colors: {
+        zn: '#bada55',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': {
+            transform: 'rotate(-3deg)'
+          },
+          '50%': {
+            transform: 'rotate(3deg)'
+          },
+        }
+      }
+    }
+  },
+  plugins: [],
+}
